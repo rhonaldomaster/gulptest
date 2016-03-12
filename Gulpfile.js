8fabee2807ace65c7413d596bf9522aa823f3221
rhonaldomaster <rhonaldomaster@gmail.com>
2016-03-12 11:15:22 -0500

    Cambios funcionalidad gulp task
    Cambios funcionalidad gulp task

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,9 +10,9 @@ var sassOptions = {
 gulp.task('sass', function () {
   console.log("Generando css desde scss");
   return gulp
-    .src('./scss/*.scss')
+    .src('scss/*.scss')
     .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(gulp.dest('./'+distfolder+'/css'));
+    .pipe(gulp.dest(distfolder+'/css'));
 });
 gulp.task('watch', function() {
   return gulp
@@ -30,23 +30,22 @@ gulp.task('htmldist',function() {
 gulp.task('jsdist',function() {
   console.log("Copiando js a dist");
   return gulp
-  .src('./js/*.js')
+  .src('js/*.js')
   .pipe(gulp.dest(distfolder+'/js'));
 });
 gulp.task('cssdist',function() {
   console.log("Copiando css a dist");
   return gulp
-  .src('./css/*.css')
+  .src('css/*.css')
   .pipe(gulp.dest(distfolder+'/css'));
 });
 gulp.task('imgdist',function() {
   console.log("Copiando img a dist");
   return gulp
-  .src('./img/*.+(png|jpg|jpeg|gif|svg|bmp)')
+  .src('img/*.+(png|jpg|jpeg|gif|svg|bmp)')
   .pipe(gulp.dest(distfolder+'/img'));
 });
 gulp.task('clean:dist', function() {
   return del.sync(distfolder);
 });
-gulp.task('default', ['sass', 'watch']);
-gulp.task('todist',['sass','htmldist','cssdist','jsdist','imgdist']);
+gulp.task('default',['sass','htmldist','cssdist','jsdist','imgdist']);
